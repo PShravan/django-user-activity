2a4a7935a7bfab69fdbf90a469a8cb39b6aab56a
PShravan <shravan.nani18@gmail.com>
2020-09-27 01:01:13 +0530

    added home page to user activities api url

--- users/views.py
+++ users/views.py
@@ -1,16 +1,15 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import *
 from django.contrib.auth.models import User
+from .models import *
+
 # Create your views here.
 
 def home(request):
-    html="<html><body>go to this <a href='/api/users/activities/'>link</a> for user activities data</body></html>"
-    return HttpResponse(html)
+    return redirect('users-activity')
 
 @api_view(['GET',])
 def users_activity(request):
